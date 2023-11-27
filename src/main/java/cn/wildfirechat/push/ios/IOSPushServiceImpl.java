33a775ea702e208fc13f6917f384b1eb57493a71
fightroad <39027157+fightroad@users.noreply.github.com>
2023-11-27 18:29:12 +0800

    unchat插件支持设置pushtype 不再需要。Revert "修改ios pushtype=1的时候也使用个推。解决unichat插件不支持设置pushtype值，导致无办法使用个推通道"
    This reverts commit 3add4d0e8770ada549f2823343b2cce83da05587.

--- src/main/java/cn/wildfirechat/push/ios/IOSPushServiceImpl.java
+++ src/main/java/cn/wildfirechat/push/ios/IOSPushServiceImpl.java
@@ -43,12 +43,10 @@ public class IOSPushServiceImpl implements IOSPushService {
                 LOG.error("等待太久，消息抛弃");
                 return;
             }
-			//改动适配个推ios，解决unichat插件不支持将pushtype设置为7。将1也执行到个推通道。
-            if(pushMessage.pushType == 0) {
+            if(pushMessage.pushType < 3) {
                 apnsServer.pushMessage(pushMessage);
-            } else if(pushMessage.pushType == 1 || pushMessage.pushType == 7){
+            } else if(pushMessage.pushType == AndroidPushType.ANDROID_PUSH_TYPE_GETUI) {
                 getuiPush.push(pushMessage, false);
-				
             } else {
                 LOG.error("Unknown ios push type: {}", pushMessage.pushType);
             }
